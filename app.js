f97ac3e452676e32e31c7d00a1983f97817b85b5
MuhammadAnas-01 <muhammadanas0819@gmail.com>
2020-06-23 21:47:16 -0700

    JS-ASSIGNMENT-6

---
+++ app.js
@@ -0,0 +1,57 @@
+// STRING METHODS -- ASSIGNMENT 21-25 //
+
+// TASK-1 //
+
+// var firstName = prompt("Enter your first Name ")
+// var lastName = prompt("Enter your last Name ")
+// var fullName = firstName + " " + lastName
+// alert("welcome " + fullName);
+
+// TASK-2 //
+
+// var favouriteMobile = prompt("Your favourite phone is ? ")
+// var lengthSting = favouriteMobile.length
+// alert("Length of your input is " + lengthSting);
+
+// TASK-3 //
+
+// var _string = "Pakistani"
+// var indexno = _string.indexOf("n");
+// alert("Index of 'n' is " + indexno);
+
+// TASK-4 //
+
+// var _string = "Hello World";
+// var indexno = _string.lastIndexOf("l");
+// alert("index of 'l' is " + indexno);
+
+// TASK-5 //
+
+// var _string = "Pakistani"
+// var indexno = _string.charAt("3");
+// alert("CHARACTER AT INDEX '3' is " + indexno);
+
+// TASK-6 //
+
+// var firstName = prompt("Enter your first Name ")
+// var lastName = prompt("Enter your last Name ")
+// var fullName = firstName.concat( " " , lastName)
+// alert("welcome " + fullName);
+
+// TASK-7 //
+
+// var _string = "Hyderabad"
+// var newstring = _string.replace("Hyder" , "Islam")
+// alert(newstring);
+
+// TASK-8 //
+
+// var message = "Ali and Sami are best friends  They play cricket and football together "
+// var newMessage = message.replace(/and/g , "&")
+// alert(newMessage)
+
+// TASK-9 //
+
+
+
+
